4085c195bf125b1504594db2cd1a9c4d3bf62690
Michaela Servi <michaelaservi@gmail.com>
2014-10-05 18:31:32 +0200

    cacca

--- semi_supervised_svc.py
+++ semi_supervised_svc.py
@@ -19,8 +19,8 @@ def svc_amino(X, y, score_type):
     :param y:
     :param score_type:
     """
-    C = 1.0  # SVM regularization parameter
-    rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C)
+    C = 70  # SVM regularization parameter
+    rbf_svc = svm.SVC(kernel='rbf', gamma=0.07, C=C)
 
     if (score_type=="split"):
         X_1,y_1=pattern_generator.load_dataset("binary",True,"86")
@@ -28,7 +28,26 @@ def svc_amino(X, y, score_type):
         X_1 = np.dot(X_1,W)
         X_train, X_test, y_train, y_test = X , X_1 , y, y_1
         rbf_svc.fit(X_train, y_train)
-        return rbf_svc.score(X_test, y_test)
+        y_score = np.array(rbf_svc.predict(X_test))
+        y_test = np.array(y_test)
+        tn = 0.0
+        fp = 0.0
+
+        for i in range(y_score.shape[0]):
+            if y_test[i]==-1:
+                if y_score[i]==-1:
+                    tn = tn+1
+                else: fp = fp+1
+        spec = tn/(tn+fp)
+        print "sensitivity:"
+        print recall_score(y_test,y_score)
+        print "specificity:"
+        print spec
+        print "accuracy:"
+        print accuracy_score(y_test,y_score)
+        print "MCC:"
+        print matthews_corrcoef(y_test,y_score)
+        return "ciao"
         #con binary fa 0.78 con pssm fa 0.80
     else:
         if(score_type=="cross"):
@@ -59,16 +78,12 @@ def train_model(model_type):
 
 if __name__=="__main__":
 
-    # W = train_model("dae")
-    # X,y=pattern_generator.load_dataset("binary",True,"353")
-    # # X,y = pattern_generator.dae_input_generator(True)
-    # X_new = np.dot(X,W)
-    # # X_new2 = pattern_generator.dae_output_resizeing(X_new)
-    # print svc_amino(X_new,y,"split")
-    W_1,W_2,W_3 = stacked_dae.train_stack()
+    W = train_model("rbm")
     X,y = pattern_generator.load_dataset("binary",True,"86")
-    X_new = np.dot(np.dot(np.dot(X,W_1),W_2),W_3)
-    print svc_amino(X_new,y,"cross")
+    X_new = np.dot(X,W)	
+   
+   
+    print svc_amino(X_new,y,"split")
     #0.853440662998 score con grande dataset 439 per training e SVM con training dae sempre su 439
     #0.77 su dataset di 86 con dae
     #0.795137504982 rbm con dataset binario allenato con 86
